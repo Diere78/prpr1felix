dd7c8dfb637e193292c493140d4f926e92ef7c0f
Felix Ö <felix.ortlund@elev.ga.ntig.se>
2018-09-05 14:52:28 +0200

    update face

--- src/prpr1felix/face.java
+++ src/prpr1felix/face.java
@@ -3,7 +3,7 @@ package prpr1felix;
 public class face {
 
 	public static void main(String[] args) {
-		System.out.println(" +wwwww+ ");
+		System.out.println(" +\"\"\"\"\"+ ");
 	    System.out.println("[| o o |]");
 		System.out.println(" |  ^  | ");
 		System.out.println(" | '-' | ");
