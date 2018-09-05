f55479cbf6c0d445f79edd0bc2b48659c9a9558b
Felix Ö <felix.ortlund@elev.ga.ntig.se>
2018-09-05 15:03:15 +0200

    felixupg8

---
+++ src/prpr1felix/twinkle.java
@@ -0,0 +1,15 @@
+package prpr1felix;
+
+public class twinkle {
+
+	public static void main(String[] args) {
+		System.out.println("Twinkle twinkle, little star,"
+				+ "\nHow i wonder what you are!"
+				+ "\nUp above the world so high,"
+				+ "\nLike a diamond in the sky."
+				+ "\nTwinkle, twinkle, little star,"
+				+ "\nHow I wonder what yolu are");
+
+	}
+
+}
