900753d410703ccd41204e71ba3ad6b0c7423800
Felix Ö <felix.ortlund@elev.ga.ntig.se>
2018-09-05 14:06:50 +0200

    java
    den är nära nog

---
+++ src/prpr1felix/java.java
@@ -0,0 +1,10 @@
+package prpr1felix;
+
+public class java {
+
+	public static void main(String[] args) {
+		System.out.println("    J    a   v     v  a \n  J   a a   v   v  a a\nJ  J  aaaaa   V V  aaaaa\nJJ  a     a   V  a     a ");
+
+	}
+
+}
