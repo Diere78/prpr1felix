9a7de4c8ad8a8f2ea8e8607a3b5df588fc61122e
Felix Ö <felix.ortlund@elev.ga.ntig.se>
2018-09-14 10:11:42 +0200

    trial1felix

---
+++ src/felixprprpr/trial1.java
@@ -0,0 +1,11 @@
+package felixprprpr;
+
+public class trial1 {
+
+	public static void main(String[] args) {
+		System.out.println(Math.round(8d/3));
+		System.out.println(Math.round(5d/4));
+
+	}
+
+}
