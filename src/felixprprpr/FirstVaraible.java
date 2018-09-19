ca9b48c3c2086aa9cd562402bbeff3d1517c6391
Felix Ö <felix.ortlund@elev.ga.ntig.se>
2018-09-19 14:20:29 +0200

    trial4

---
+++ src/felixprprpr/FirstVaraible.java
@@ -0,0 +1,20 @@
+package felixprprpr;
+
+public class FirstVaraible {
+
+	public static void main(String[] args) {
+		
+		int i = 6;
+		double d = 6.6 ;
+		boolean bool = i == d ; 
+		
+		
+		System.out.println( bool);
+		
+		}
+			
+	
+
+	}
+
+
---
+++ src/felixprprpr/trial4.java
@@ -0,0 +1,12 @@
+package felixprprpr;
+
+public class trial4 {
+
+	public static void main(String[] args) {
+		double a = 123.45;
+	    double roundOff = Math.floor(a * 100d)/100.d; 
+		System.out.println(roundOff);
+
+	}
+
+}
