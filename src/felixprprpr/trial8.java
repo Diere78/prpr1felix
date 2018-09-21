90c49f7d8924ff35ec7a6cc64109b96f263d6320
Felix Ö <felix.ortlund@elev.ga.ntig.se>
2018-09-21 10:54:36 +0200

    trial8 felix ö

---
+++ src/felixprprpr/trial8.java
@@ -0,0 +1,11 @@
+package felixprprpr;
+
+public class trial8 {
+
+	public static void main(String[] args) {
+		float intrestRate = -12.25f; 
+         
+		System.out.println(Math.abs(intrestRate));
+	}
+
+}
