b9a5d2aece7fff9be8d253715d9ee2396d2fa89a
Felix Ö <felix.ortlund@elev.ga.ntig.se>
2018-09-21 11:14:14 +0200

    trial9 felix

---
+++ src/felixprprpr/trial9.java
@@ -0,0 +1,12 @@
+package felixprprpr;
+
+public class trial9 {
+
+	public static void main(String[] args) {
+		float flo = 12.25f;		
+		System.out.println(flo);
+		System.out.println((float) Math.round(flo));
+
+	}
+
+}
