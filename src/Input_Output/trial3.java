e53b27b28a4366a0be651bc7be9a0acd115391af
Felix Ö <felix.ortlund@elev.ga.ntig.se>
2018-09-28 09:17:08 +0200

    trial3felix

---
+++ src/Input_Output/trial3.java
@@ -0,0 +1,25 @@
+package Input_Output;
+import java.util.Scanner;
+
+public class trial3 {
+
+	public static void main(String[] args) {
+		Scanner inputScanner = new Scanner(System.in);
+		
+		System.out.println("Enter nr: ");		
+		int nr1 = inputScanner.nextInt();
+		
+		System.out.println(nr1 * 1 );
+		System.out.println(nr1 * 2 );
+		System.out.println(nr1 * 3 );
+		System.out.println(nr1 * 4 );
+		System.out.println(nr1 * 5 );
+		System.out.println(nr1 * 6 );
+		System.out.println(nr1 * 7 );
+		System.out.println(nr1 * 8 );
+		System.out.println(nr1 * 9 );
+		System.out.println(nr1 * 10 );
+
+	}
+
+}
