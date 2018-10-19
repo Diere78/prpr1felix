7f34260424da292879dfc5e70e6a812466577928
Felix Ö <felix.ortlund@elev.ga.ntig.se>
2018-10-19 09:24:28 +0200

    Gjord rätt

--- src/Input_Output/trial7.java
+++ src/Input_Output/trial7.java
@@ -6,17 +6,22 @@ public class trial7 {
 	public static void main(String[] args) {
 		Scanner inputScanner = new Scanner(System.in);
 		
-		int a, b, c;
+		int c;
 		
-		a = 10; 
-		b= 25;
+		System.out.println("Enter nr1: ");		
+		int a = inputScanner.nextInt();
 		
+		System.out.println("Enter nr: ");		
+		int b = inputScanner.nextInt(); 
+		
+		System.out.println(" a b");
 		System.out.println(" " + a + " " + b);
 		
 		c = a; 
 		a = b;
 		b = c;
-		System.out.println(" " + a + " " + b);
+		System.out.println("new a and b");
+		System.out.println("    " + a + "     " + b);
  
 	}
 
