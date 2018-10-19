e61295922caaddff0cde18e6331d7eda7c42fc3c
Felix Ö <felix.ortlund@elev.ga.ntig.se>
2018-10-19 11:01:04 +0200

    done

--- src/Looop/SumAndAverage_1.java
+++ src/Looop/SumAndAverage_1.java
@@ -2,22 +2,22 @@ package Looop;
 
 import java.util.Scanner;
 
-public class SumAndAvarage {
+public class SumAndAverage_1 {
 
 	public static void main(String[] args) {
 		Scanner inputScanner = new Scanner(System.in);
 		int sum = 0;
-		int j = 0;
+		int j = 1;
 
 		System.out.println("Enter Number: ");
 		int nr = inputScanner.nextInt();
 
-		for (int i = 1; i <= nr; i++) {
-			sum += i;
+		while (j <= nr) {
+			sum += j;
 			System.out.println(sum);
-			j = i;
+			j++;
 		}
-		System.out.println("Average = " + sum + " / " + j + " = " + (double) sum / (double) j);
+		System.out.println("Average = " + sum + " / " + nr + " = " + (double) sum / (double) nr);
 
 	}
 
